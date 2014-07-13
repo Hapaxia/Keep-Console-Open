8cf902c714dac9ff6436e8a6f7479f4608aafbaf
Hapax <MJSilk2@gmail.com>
2014-07-13 03:33:34 +0100

    Added example
    Console-only example. Still requires SFML.

---
+++ example.cpp
@@ -0,0 +1,28 @@
+/*
+KeepConsoleOpen example
+by Hapax (github.com/hapaxia)
+July 2014
+
+As the variable, zero, is a value of 1 (i.e. not 0), "Not zero!" is output to the console and then the application exits. The console is kept open until a key is pressed.
+If you change the "zero = 1;" to "zero = 0;", "Perfectly zero." is output to the console before the application exits. The console is no longer held open.
+*/
+
+#include <iostream>
+#include "KeepConsoleOpen.h"
+
+main()
+{
+	KeepConsoleOpen keepConsoleOpen;
+
+	int zero = 1;
+	if (zero == 0)
+		std::cout << "Perfectly zero." << std::endl;
+	else // zero != 0
+	{
+		std::cout << "Not zero!" << std::endl;
+		return EXIT_FAILURE;
+	}
+
+	keepConsoleOpen.allowToClose();
+	return EXIT_SUCCESS;
+}
